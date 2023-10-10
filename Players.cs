7e0fad16c3974a5c83a7ee747ecbcfaa7f74febc
DylanGrampp <114005528+DylanGrampp@users.noreply.github.com>
2023-10-10 04:34:49 -0400

    Add files via upload

---
+++ Players.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab5API
+{
+    internal class Players
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty; 
+        public string Username { get; set; } = string.Empty;
+        public int Player_ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public int Joined { get; set; }
+        public int Last_Online { get; set; }
+        public int Followers { get; set; }
+        public bool Is_Streamer { get; set; }
+        public string Twitch_Url { get; set; } = string.Empty;
+        public string League { get; set; } = string.Empty;
+
+
+        public Players() { }
+
+        public Players(string name, string url, string username, int player_id, string title, string status, string location, string country, int joined, int last_online, int followers, bool is_streaner, string twitch_url, string league)        
+        {
+            Name = name;
+            Url = url;
+            Username = username;
+            Player_ID = player_id;
+            Title = title;
+            Status = status;
+            Location = location;
+            Country = country;
+            Joined = joined;
+            Last_Online = last_online;
+            Followers = followers;
+            Is_Streamer = is_streaner;
+            Twitch_Url = twitch_url;
+            League = league;
+        }
+
+        public override string ToString()
+        {
+
+            string msg = "";
+
+            msg += "\n---------------------------------------------------\n";
+
+            msg += $"Now viewing user: {Username} \n";
+            msg += $"Last Online (GMT): {DateTime.UnixEpoch.AddSeconds(Last_Online)}\n";
+
+            msg += $"\nPlayer name: {Name}\n";
+            msg += $"Location: {Location}\n";
+            msg += $"Followers: {Followers}\n";
+            msg += $"Chess title: {Title}\n";
+            msg += $"Current League: {League}\n";
+            if (Is_Streamer == true)
+            {
+                msg += $"Streamer: Yes\n{Twitch_Url}\n";
+            }
+            else
+            {
+                msg += "Streamer: No\n";
+            }
+            //msg += $"Account status: {Status}\n";
+            //msg += $"Followers: {Followers}\n";
+
+
+            msg += $"\nAccount status: {Status}\n";
+            msg += $"Account created (GMT): {DateTime.UnixEpoch.AddSeconds(Joined)}\n";
+            /*if(Is_Streamer == true)
+            {
+                msg += $"Streamer: Yes\n";
+            }
+            else
+            {
+                msg += "Streamer: No\n";
+            }*/
+            //msg += $"Streamer: {Is_Streamer}";
+            //msg += $"Last Online (GMT): {DateTime.UnixEpoch.AddSeconds(Last_Online)}\n";
+
+            msg += "\nExtra Info\n";
+            msg += $"Player ID: {Player_ID}\n";
+            msg += $"Account country: {Country}\n";
+            msg += $"Link to account: {Url}\n";
+
+            msg += "---------------------------------------------------";
+
+            /* msg += "\n     Chess.com API\n     Programmed by: Dylan Grampp";
+            msg += "\n     -----------------------------\n";
+            msg += "     1. View a profile\n";
+            msg += "     2. Player stats\n";
+            msg += "     3. Leaderboard\n";
+            msg += "     4. Exit program\n";
+            msg += $"     \nPlease enter the number of your choice: ";
+           */
+
+            return msg; 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Lab5API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+           await Display();
+        }
+        
+        public static async Task Display()
+        {
+
+            int menuInput;
+            do
+            {
+                MainMenu();
+                menuInput = Convert.ToInt32(Console.ReadLine());
+
+                switch (menuInput)
+                {
+                    case 1:
+                       await Profile();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        break;
+
+                }
+            } while (menuInput != 4);
+        }
+
+
+        public static async void MainMenu()
+        {
+            Console.WriteLine("\n     Chess.com API\n     Programmed by: Dylan Grampp");
+            Console.WriteLine("     -----------------------------\n");
+            Console.WriteLine("     1. View a profile");
+            Console.WriteLine("     2. Player stats");
+            Console.WriteLine("     3. Leaderboard");
+            Console.WriteLine("     4. Exit program\n");
+            Console.Write($"     Please enter the number of your choice: ");
+        }
+
+        public static async Task Profile()
+        {
+            Console.Write("\nEnter the username of the chess account you would like to lookup: ");
+            string username = Console.ReadLine();
+
+            Console.Clear();
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("User-Agent", "Authentication");
+
+            HttpResponseMessage response = await client.GetAsync($"https://api.chess.com/pub/player/{username}");
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+            Players p = JsonSerializer.Deserialize<Players>(json, options);
+            Console.WriteLine(p);
+
+            Console.WriteLine("\nProfile loaded. Press Enter to return to the main menu.");
+            Console.ReadLine();
+
+            Console.Clear();
+        }
+    }
+}
